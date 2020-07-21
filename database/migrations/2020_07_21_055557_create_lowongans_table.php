375b3e8f280e460955db5aa4701b9434f2ff6b88
cosetha <cosethapun@gmail.com>
2020-07-21 13:01:33 +0700

    Membuat migrasi database beserta model nya bagian C rev-1

---
+++ database/migrations/2020_07_21_055557_create_lowongans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLowongansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lowongan', function (Blueprint $table) {
+            $table->id();
+            $table->text('gambar');
+            $table->string('nama_lowongan',255);
+            $table->text('deskripsi');
+            $table->bigInteger('kode_jenis')->unsigned();
+            $table->foreign('kode_jenis')->references('id')->on('jenis_lowongan')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lowongan');
+    }
+}
