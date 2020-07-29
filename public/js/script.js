ae3ad8a4e6f6289c3f3174ca5f2f81d06b2129c5
andriSH30 <andrisetya93@gmail.com>
2020-07-30 01:38:58 +0800

    membuat fitur jabatan

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+//------------------------------------------FITUR JABATAN------------------------------------------
+
   //DATATABLE JABATAN
   LoadTableJabatan()
   function LoadTableJabatan(){
@@ -141,5 +143,7 @@ $(document).ready(function() {
       })
     })
   })
+
+//------------------------------------------END FITUR JABATAN-----------------------------------------
 
 });
