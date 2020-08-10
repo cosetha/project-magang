47b6a93ee153a67218e47ed6bb17d78c632dc50c
bagasfa <bagasmgs@gmail.com>
2020-08-10 14:22:43 +0700

    Reset Password via Email

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,6 @@ use Illuminate\Support\Facades\Route;
 //ROUTER GUEST
 Route::group(['middleware' => 'guest'],function(){
 
-Route::get('/forget', function () {
-    return view('admin/forgetpassword');
-});
-
-
     Route::get('/','PageController@LoginForm');
 
 });
