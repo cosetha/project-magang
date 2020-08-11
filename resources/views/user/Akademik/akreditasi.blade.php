82e6f7faac2f9b0a3e278c2394f537c3594a523c
andriSH30 <andrisetya93@gmail.com>
2020-08-11 20:53:10 +0800

    Implementasi Halaman user dari frontend

---
+++ resources/views/user/Akademik/akreditasi.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts/userlayout')
+@section('title', 'Fasilitas')
+
+@section('content')
+
+
+<div class="container-fluid background">
+    <div class="row mt-4">
+        <div class="col berita">
+            Akreditasi
+        </div>
+    </div>
+</div>
+
+<div class="container my-5">
+    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+    <table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+
+<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+<button type="button" class="btn btn-primary">Primary</button>
+
+</div>
+
+@endsection
---
+++ resources/views/user/BK/bidangkeahlian.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts/userlayout')
+@section('title', 'Fasilitas')
+
+@section('content')
+
+<div class="container-img">
+  <img src="{{ asset('img/rog.jpg') }}" alt="Snow" style="width:100%; height:400px ;">
+  <div class="bottom-left">Sistem Informasi</div>
+  <div class="top-left">Top Left</div>
+  <div class="top-right">Top Right</div>
+  <div class="bottom-right">Bottom Right</div>
+  <div class="centered">Centered</div>
+</div>
+
+<div class="container">
+    <div class="row mt-4 mb-row">
+            <div class="col mx-4 col-overflow">
+                <h2>Visi</h2>
+
+            </div>
+
+
+
+             <div class="col mx-4 col-overflow">
+                <h2>Misi</h2>
+            </div>
+
+        </div>
+</div>
+
+@endsection
