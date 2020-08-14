2bb620b596b0dbeab571d396134a52af21c8daa0
bagasfa <bagasmgs@gmail.com>
2020-08-14 12:41:52 +0700

    fix Btn Cancel

--- resources/views/layouts/adminLayout.blade.php
+++ resources/views/layouts/adminLayout.blade.php
@@ -340,7 +340,11 @@
                                     <li class="nav-item dropdown no-arrow">
                                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                             <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->name }}</span>
-                                            <img class="img-profile rounded-circle" src="{{ asset('img/profile') }}/{{ auth()->user()->gambar }}">
+                                            @if(auth()->user()->gambar == !NULL)
+                                                <img class="img-profile rounded-circle" src="{{ asset('img/profile') }}/{{ auth()->user()->gambar }}">
+                                            @else
+                                                <img class="img-profile rounded-circle" src="{{ asset('img/Login-image.png') }}">
+                                            @endif
                                         </a>
                                         <!-- Dropdown - User Information -->
                                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
