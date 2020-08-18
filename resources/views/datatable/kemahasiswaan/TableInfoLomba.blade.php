6613437739ccc8632a9d94dc8a483e21f2f6f3f5
bagasfa <bagasmgs@gmail.com>
2020-08-18 10:57:18 +0700

    Menu Info Lomba + Revisi DB

---
+++ resources/views/datatable/kemahasiswaan/TableInfoLomba.blade.php
@@ -0,0 +1,12 @@
+<table class="table table-bordered" id="infoLomba-table" width="100%" cellspacing="0">
+  <thead>
+    <tr align="center">
+      <th>No</th>
+      <th>Judul</th>
+      <th>Deskripsi</th>
+      <th>Lokasi</th>
+      <th>Tanggal</th>
+      <th>Action</th>
+    </tr>
+  </thead>
+</table>
