0b871dfc69425eec7a9524134e168159e207bd2c
djavu <setyawandicky88@gmail.com>
2020-08-26 11:41:37 +0700

    fix bug upload gambar dan tambah detail

--- routes/web.php
+++ routes/web.php
@@ -303,6 +303,7 @@ Route::group(['middleware' => ['auth','checkRole:1,2']],function(){
         Route::get('edit/{id}', 'Home\BeritaController@edit');
         Route::get('delete/{id}', 'Home\BeritaController@destroy');
         Route::post('update/{id}', 'Home\BeritaController@update');
+        Route::get('show/{id}', 'Home\BeritaController@show');
     });
 
     Route::prefix('agenda')->group(function () {
@@ -332,6 +333,7 @@ Route::group(['middleware' => ['auth','checkRole:1,2']],function(){
         Route::get('edit/{id}', 'Home\PengumumanController@edit');
         Route::post('update/{id}', 'Home\PengumumanController@update');
         Route::get('delete/{id}', 'Home\PengumumanController@destroy');
+        Route::get('show/{id}', 'Home\PengumumanController@show');
     });
 
     //Kemahasiswaan
