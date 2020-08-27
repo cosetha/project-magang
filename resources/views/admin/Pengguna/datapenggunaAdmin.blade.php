e87a3532f635db8e5c60409818c4375a2fd338c3
bagasfa <bagasmgs@gmail.com>
2020-08-27 20:52:30 +0700

    Export DataPengguna

--- resources/views/admin/Pengguna/datapenggunaAdmin.blade.php
+++ resources/views/admin/Pengguna/datapenggunaAdmin.blade.php
@@ -12,8 +12,10 @@
 
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Pengguna</h1>
-        <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>-->
+        <h1 class="h3 mb-0 text-gray-800">Data Pengguna</h1>
+        <!-- Export Data -->
+         <a href="{{url('datapengguna/export')}}" class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm"><i class="fas fa-file-excel mr-2"></i>Export Data</a>
+         <!-- Export Data -->
     </div>
 
     <!-- Content Row -->
