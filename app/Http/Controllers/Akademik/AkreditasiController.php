490d3e5f267bff3a7a892f02633ecb9144045ae1
bagasfa <bagasmgs@gmail.com>
2020-09-17 12:53:23 +0700

    Re-Upload Akreditasi

--- app/Http/Controllers/Akademik/AkreditasiController.php
+++ app/Http/Controllers/Akademik/AkreditasiController.php
@@ -32,7 +32,7 @@ class AkreditasiController extends Controller
             "file" => 'required|mimes:jpeg,jpg,png|max:8192',
             'nilai' => 'required',
             "tanggal_mulai" => 'required',
-        	"tanggal_selesai" => 'required'], $messages);
+            "tanggal_selesai" => 'required'], $messages);
         if($validator->fails()){
             $error = $validator->errors()->first();
                 return response()->json([
@@ -55,10 +55,10 @@ class AkreditasiController extends Controller
                 $akreditasi->save();
 
                 $history = new Histori;
-                    $history->nama = auth()->user()->name;
-                    $history->aksi = "Tambah";
-                    $history->keterangan = "Menambahkan Akreditrasi Tahun '".$request->tanggal_mulai." - ".$request->tanggal_selesai."'";
-                    $history->save();
+                $history->nama = auth()->user()->name;
+                $history->aksi = "Tambah";
+                $history->keterangan = "Menambahkan Akreditrasi Tahun '".$request->tanggal_mulai." - ".$request->tanggal_selesai."'";
+                $history->save();
 
                 return response()->json([
                     'message' => 'Success'
@@ -92,7 +92,7 @@ class AkreditasiController extends Controller
 
     public function update(Request $request, $id)
     {
-    	$akreditasi = Akreditasi::find($id);
+        $akreditasi = Akreditasi::find($id);
         $messages = array(
             'file.mimes' => 'File akreditasi Perlu di Isi dengan Format: jpeg,jpg,png',
             'file.max' => 'Ukuran File Maksimal 8 MB'
@@ -149,17 +149,17 @@ class AkreditasiController extends Controller
 
                     unlink($akreditasi->file);
 
-	                $akreditasi->file = $directory."/".$nama;
-	                $akreditasi->nilai = $request->nilai;
-	                $akreditasi->tanggal_mulai=$request->tanggal_mulai;
-	                $akreditasi->tanggal_selesai=$request->tanggal_selesai;
-	                $akreditasi->save();
+                    $akreditasi->file = $directory."/".$nama;
+                    $akreditasi->nilai = $request->nilai;
+                    $akreditasi->tanggal_mulai=$request->tanggal_mulai;
+                    $akreditasi->tanggal_selesai=$request->tanggal_selesai;
+                    $akreditasi->save();
                 }else{
-	                $akreditasi = Akreditasi::find($id);
-	                $akreditasi->nilai = $request->nilai;
-	                $akreditasi->tanggal_mulai=$request->tanggal_mulai;
-	                $akreditasi->tanggal_selesai=$request->tanggal_selesai;
-	                $akreditasi->save();
+                    $akreditasi = Akreditasi::find($id);
+                    $akreditasi->nilai = $request->nilai;
+                    $akreditasi->tanggal_mulai=$request->tanggal_mulai;
+                    $akreditasi->tanggal_selesai=$request->tanggal_selesai;
+                    $akreditasi->save();
                 }
 
             } catch (\Exception $e) {
@@ -180,10 +180,10 @@ class AkreditasiController extends Controller
 
             $akreditasi = Akreditasi::find($id);
             $history = new Histori;
-                        $history->nama = auth()->user()->name;
-                        $history->aksi = "Hapus";
-                        $history->keterangan = "Menghapus Akreditasi Tahun '".$akreditasi->tanggal_mulai." - ".$akreditasi->tanggal_selesai."'";
-                        $history->save();
+            $history->nama = auth()->user()->name;
+            $history->aksi = "Hapus";
+            $history->keterangan = "Menghapus Akreditasi Tahun '".$akreditasi->tanggal_mulai." - ".$akreditasi->tanggal_selesai."'";
+            $history->save();
             $akreditasi->delete();
             return response()->json([
                 "message" => "Success"
@@ -199,10 +199,11 @@ class AkreditasiController extends Controller
         DB::table('akreditasi')->update(array('status' => 'nonaktif'));
 
         $a = Akreditasi::find($id);
+        $history = new Histori;
         $history->nama = auth()->user()->name;
-                        $history->aksi = "Mengaktifkan";
-                        $history->keterangan = "Mengaktifkan Akreditasi Tahun '".$a->tanggal_mulai." - ".$a->tanggal_selesai."'";
-                        $history->save();
+        $history->aksi = "Mengaktifkan";
+        $history->keterangan = "Mengaktifkan Akreditasi Tahun '".$a->tanggal_mulai." - ".$a->tanggal_selesai."'";
+        $history->save();
         $a->status = "aktif";
         $a->save();
 
@@ -213,10 +214,11 @@ class AkreditasiController extends Controller
 
     public function nonAktifkan($id){
         $a = Akreditasi::find($id);
+        $history = new Histori;
         $history->nama = auth()->user()->name;
-                        $history->aksi = "Menonaktifkan";
-                        $history->keterangan = "Menonaktifkan Akreditasi Tahun '".$a->tanggal_mulai." - ".$a->tanggal_selesai."'";
-                        $history->save();
+        $history->aksi = "Menonaktifkan";
+        $history->keterangan = "Menonaktifkan Akreditasi Tahun '".$a->tanggal_mulai." - ".$a->tanggal_selesai."'";
+        $history->save();
         $a->status = "nonaktif";
         $a->save();
 
