13c04d7a1faf8fb144e87df47562e472a881edb7
bagasfa <bagasmgs@gmail.com>
2020-09-17 10:42:50 +0700

    History Export

--- resources/views/admin/Histori/histori.blade.php
+++ resources/views/admin/Histori/histori.blade.php
@@ -2,7 +2,6 @@
 @section('title', 'History')
 
 @section('content')
-
 <script type="text/javascript">
     document.getElementById('history').classList.add('active');
 </script>
@@ -20,7 +19,9 @@
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">DataTables History</h6>
         </div>
-
+        <div class="d-sm-flex align-items-center m-3">
+            <a type="submit" class="btn btn-primary ml-2" href="#" data-toggle="modal" data-target="#exportHistory"><i class="fas fa-file-export"></i> Export </a>
+        </div>
         <div class="card-body">
             <div id="datatable-history"></div>
         </div>
@@ -29,7 +30,24 @@
 </div>
 <!-- End of Main Content -->
 
-
+<!-- Export History Modal-->
+<div class="modal fade" id="exportHistory" tabindex="-1" role="dialog" aria-labelledby="ModalExport" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="ModalExport">Export Data</h5>
+                <button class="close btn-close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">Pilih Format Laporan untuk di Export</div>
+            <div class="modal-footer">
+                <a href="/history/export-excel" class="btn btn-outline-success"><i class="fas fa-file-excel"></i> Excel</a>
+                <a href="/history/export-pdf" class="btn btn-outline-danger"><i class="fas fa-file-pdf"></i> PDF</a>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 
 @section('js-ajax')
