586eb2d957f87f0be8843d814c9110678f3152eb
BoySandyUtomo <60342771+BoySandyUtomo@users.noreply.github.com>
2020-10-27 05:32:21 +0800

    Revisi Home Dan Profile V1.2

--- resources/views/layouts/userlayout.blade.php
+++ resources/views/layouts/userlayout.blade.php
@@ -11,22 +11,24 @@
     <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
-    <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet">
     <link href="{{asset('css/style.css')}}" rel="stylesheet">
-    <link href="{{asset('css/style-baru.css')}}" rel="stylesheet">
+    <link href="{{asset('css/profile.css')}}" rel="stylesheet">
      <link href="{{asset('css/style-baru2.css')}}" rel="stylesheet">
     <link href="{{asset('css/font-baru.css')}}" rel="stylesheet">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <script src="{{asset('js/javascript.js')}}"></script>
 </head>
 
 <body>
 <nav class="navbar navbar-expand-lg nav-quick text-white navbar-color">
   <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-    <ul class="navbar-nav quick-margin font1">
+    <ul class="navbar-nav quick-margin">
 
     <li class="nav-item">
       <a class="nav-link" href="">UB Official</span></a>
@@ -48,8 +50,8 @@
   
 <div class="container-fluid background">
   <div class="row">
-      <div class="col kotak1 mt-4">
-        <img class="kotak1" src="{{ asset('img/teknologi informasi.png') }}" >
+      <div class="col kotak mt-4">
+        <img src="{{ asset('img/teknologi informasi.png') }}" class="kotak">
       </div>
   </div>
 
@@ -63,9 +65,9 @@
   </button>
 
   <div class="collapse navbar-collapse" id="navbarToggler">
-  <ul class="navbar-nav mr-auto font2">
+  <ul class="navbar-nav mr-auto">
         <li class="nav-item">
-            <a class="nav-link " href="{{ url('/') }}">Home</span></a>
+            <a class="nav-link" href="{{ url('/') }}">Home</span></a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -153,15 +155,16 @@
 
         <!-- Links -->
         <div class="mx-3">
-        <img src="{{ asset('img/BKSI.png') }}" width=" 100%" height="100%">
-
+        <div class="background-white">
+          <img src="{{ asset('img/vokasi UB.png') }}">
+        </div>
         <ul class="list-unstyled mt-3">
           <li>
-            <h5 class="font5">TEKNOLOGI INFORMASI</h5>
-            <h3 class="font11">PENDIDIKAN VOKASI</h3>
-            <h5 class="font5">UNIVERSITAS BRAWIJAYA</h5>
-            <p class="font2">Jl. Veteran No.12-13 Malang Jawa Timur</p>
-            <p class="font2">ti.vokasi@ub.ac.id</p>
+            <h5>TEKNOLOGI INFORMASI</h5>
+            <h3>PENDIDIKAN VOKASI</h3>
+            <h5>UNIVERSITAS BRAWIJAYA</h5>
+            <p>Jl. Veteran No.12-13 Malang Jawa Timur</p>
+            <p>ti.vokasi@ub.ac.id</p>
           </li>
         </ul>
         </div>
@@ -171,10 +174,10 @@
       <div class="col-md-3 mx-auto">
       <div class="mx-3">
         <!-- Links -->
-        <h5 class="font-weight-bold font8 text-uppercase mt-3">Fasilitas Kampus</h5>
+        <h5 class="font-weight-bold text-uppercase mt-3">Fasilitas Kampus</h5>
         <hr >
 
-        <ul class="list-unstyled font2">
+        <ul class="list-unstyled">
           <li>
             <p>BITS UB</p>
           </li>
@@ -209,10 +212,10 @@
       <div class="col-md-3 mx-auto">
       <div class="mx-3">
         <!-- Links -->
-        <h5 class="font-weight-bold font8 text-uppercase mt-3">Kemahasiswaan</h5>
+        <h5 class="font-weight-bold text-uppercase mt-3">Kemahasiswaan</h5>
         <hr >
 
-        <ul class="list-unstyled font2">
+        <ul class="list-unstyled">
           <li>
             <p>Email Mahasiswa</p>
           </li>
@@ -242,10 +245,10 @@
       <div class="col-md-3 mx-auto">
       <div class="mx-3">
         <!-- Links -->
-        <h5 class="font-weight-bold font8 text-uppercase mt-3">Blog</h5>
+        <h5 class="font-weight-bold text-uppercase mt-3">Blog</h5>
         <hr >
 
-        <ul class="list-unstyled font2">
+        <ul class="list-unstyled">
           <li>
             <p>UB Official Web</p>
           </li>
@@ -269,12 +272,19 @@
       </div>
 
 
+      
+
+
     </div>
     <!-- Grid row -->
 
-
+    
   
 </footer>
+
+<div class="copy-right">
+  <div class="font-copyright">Copyright 2020.Allrights Reserved. Teknologi Informasi Pendidikan Vokasi UB</div>
+</div>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
