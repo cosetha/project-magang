7603df24815b7fa60251500f657af26b158b39eb
BoySandyUtomo <60342771+BoySandyUtomo@users.noreply.github.com>
2020-11-25 12:17:51 +0800

    Home Dan Profile FIx

--- resources/views/user/Profile/sejarah.blade.php
+++ resources/views/user/Profile/sejarah.blade.php
@@ -3,49 +3,110 @@
 
 @section('content')
 
-<div class="caption">
-    <img src="{{ asset('img/rog.jpg') }}" class="img-full-width" />
-    <h3>Sejarah</h3>
-</div>
 
-@foreach($head ?? '' as $item)
-  <div class="container">
-        <h2>{{$item['judul']}}</h2>
-        <p>{!! $item['deskripsi'] !!}</p>
-  </div>
-@endforeach
-<!-- <div class="timeline mt-5">
-    <div class="container-tl left">
-      <div class="content">
-        <h2>2017</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, sunt magnam quaerat laboriosam, consequatur fugit alias nemo minima nulla aut eum rerum maiores. Id beatae vel assumenda. Quos, sed expedita. Consectetur, quasi. Recusandae, consequatur illum.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt molestiae nam perspiciatis, pariatur in qui minus aperiam autem dignissimos eos.
-        </p>
+      <div class="container-margin">
+        <div class="row">
+            <div class="col-sm-8">
+                <div class="box-visimisi">
+                    <div class="navigasi font-roboto-13">
+                        Home > Profil > Struktur
+                        <div class="hr-navigasi"></div>
+                    </div>
+
+                    <div class="judul-visimisi">
+                        <div class="judul_bagian font3">
+                            Sejarah 
+                        </div>
+                        <div class="hr"></div>
+                    </div>
+
+                    <div class="container">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, sunt magnam quaerat laboriosam, consequatur fugit alias nemo minima nulla aut eum rerum maiores. Id beatae vel assumenda. Quos, sed expedita. Consectetur, quasi. Recusandae, consequatur illum.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt molestiae nam perspiciatis, pariatur in qui minus aperiam autem dignissimos eos.
+                      </p>
+                    </div>
+
+                <div class="timeline mt-5">
+                  
+                    <div class="container-tl left">
+                        <div class="content">
+                          <h2>2017</h2>
+                          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, sunt magnam quaerat laboriosam, consequatur fugit alias nemo minima nulla aut eum rerum maiores. Id beatae vel assumenda. Quos, sed expedita. Consectetur, quasi. Recusandae, consequatur illum.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt molestiae nam perspiciatis, pariatur in qui minus aperiam autem dignissimos eos.
+                          </p>
+                        </div>
+                    </div>
+
+                    <div class="container-tl right">
+                      <div class="content">
+                        <h2>2016</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nobis nemo ex minus possimus. Magnam ullam tempore dolorum voluptatibus ex?</p>
+                      </div>
+                    </div>
+
+                    <div class="container-tl left">
+                      <div class="content">
+                        <h2>2015</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nobis nemo ex minus possimus. Magnam ullam tempore dolorum voluptatibus ex?</p>
+                      </div>
+                    </div>
+
+                    <div class="container-tl right">
+                      <div class="content">
+                        <h2>2014</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nobis nemo ex minus possimus. Magnam ullam tempore dolorum voluptatibus ex?</p>
+                      </div>
+                    </div>
+
+                    <div class="container-tl left">
+                      <div class="content">
+                        <h2>2013</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nobis nemo ex minus possimus. Magnam ullam tempore dolorum voluptatibus ex?</p>
+                      </div>
+                    </div>
+
+                </div>
+
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <div class="box-menu">
+                    <div class="judul-menu font8px">
+                        PROFIL
+                    </div>
+                    <div class="menu font10">
+                        <!-- bold jika di click -->
+                        <div class="isi-menu">
+                            <a href="#" style="text-decoration:none;">
+                                Sejarah
+                            </a>
+                            <div class="hr-profile"></div>
+                        </div>
+                        <div class="isi-menu">
+                            <a href="#" style="text-decoration:none;">
+                                Visi dan Misi
+                            </a>
+                            <div class="hr-profile"></div>
+                        </div>
+                        <div class="isi-menu">
+                            <a href="#" style="text-decoration:none;">
+                                Struktur Organisasi
+                            </a>
+                            <div class="hr-profile"></div>
+                        </div>
+                        <div class="isi-menu">
+                            <a href="#" style="text-decoration:none;">
+                                Prestasi
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
-    </div>
-    <div class="container-tl right">
-      <div class="content">
-        <h2>2016</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nobis nemo ex minus possimus. Magnam ullam tempore dolorum voluptatibus ex?</p>
-      </div>
-    </div>
-    <div class="container-tl left">
-      <div class="content">
-        <h2>2015</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nobis nemo ex minus possimus. Magnam ullam tempore dolorum voluptatibus ex?</p>
-      </div>
-    </div>
-    <div class="container-tl right">
-      <div class="content">
-        <h2>2014</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nobis nemo ex minus possimus. Magnam ullam tempore dolorum voluptatibus ex?</p>
-      </div>
-    </div>
-    <div class="container-tl left">
-      <div class="content">
-        <h2>2013</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nobis nemo ex minus possimus. Magnam ullam tempore dolorum voluptatibus ex?</p>
-      </div>
-    </div>
-  </div> -->
+
+
+
+
 @endsection
--- resources/views/user/Profile/struktur.blade.php
+++ resources/views/user/Profile/struktur.blade.php
@@ -3,74 +3,64 @@
 
 @section('content')
 
-<div class="caption">
-    <img src="{{ asset('img/rog.jpg') }}" class="img-full-width" />
-    <h3>My Caption Goes Here</h3>
-</div>
 
+    <div class="container-margin">
+        <div class="row">
+            <div class="col-sm-8">
+                <div class="box-visimisi">
+                    <div class="navigasi font-roboto-13">
+                        Home > Profil > Struktur
+                        <div class="hr-navigasi"></div>
+                    </div>
 
-<div class="container">
-        <h2>Struktur Organisasi</h2>
-  </div>
-<div class="batas"></div>
+                    <div class="judul-visimisi">
+                        <div class="judul_bagian font3">
+                            Struktur Organisasi 
+                        </div>
+                        <div class="hr"></div>
+                    </div>
 
-<!-- <div class="container">
-    <h2>Pimpinan Prodi Teknologi Informasi</h2>
-    <p>lorem ipsum</p>
-    <br>
+                    <div class="kotak-struktur">    
+                        <img class="gambar-struktur" src="{{ asset('img/gambar 2.jpg') }}" >
+                    </div>
 
-    <h2 align="center">Pimpinan Prodi Teknologi Informasi</h2>
-    <hr>
-
-    <div class="card mb-3 mt-5 border-0" style="max-width: 100%;">
-        <div class="row no-gutters">
-            <div class="col-md-4">
-            <img src="{{ asset('img/rog.jpg') }}" height="100%" width="150px" style="margin-left:150px;">
+                </div>
             </div>
-            <div class="col-md-8">
-            <div class="card-body">
-                <h5 align="center" class="card-title font-weight-bold">Card title</h5>
-                <hr>
-                <h6 align="center" class="card-title font-weight-bold">Card title</h6>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-            </div>
-        </div>
-    </div>
 
-    <div class="card mb-3 mt-5 border-0" style="max-width: 100%;">
-        <div class="row no-gutters">
-            <div class="col-md-8">
-            <div class="card-body">
-                <h5 align="center" class="card-title font-weight-bold">Card title</h5>
-                <hr>
-                <h6 align="center" class="card-title font-weight-bold">Card title</h6>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-            </div>
-            <div class="col-md-4">
-            <img src="{{ asset('img/rog.jpg') }}" height="100%" width="150px" style="margin-left:150px;">
-            </div>
-        </div>
-    </div>
-
-
-
-    <div class="card mb-3 mt-5 border-0" style="max-width: 100%;">
-        <div class="row no-gutters">
-            <div class="col-md-4">
-            <img src="{{ asset('img/rog.jpg') }}" height="100%" width="150px" style="margin-left:150px;">
-            </div>
-            <div class="col-md-8">
-            <div class="card-body">
-                <h5 align="center" class="card-title font-weight-bold">Card title</h5>
-                <hr>
-                <h6 align="center" class="card-title font-weight-bold">Card title</h6>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
+            <div class="col-sm-4">
+                <div class="box-menu">
+                    <div class="judul-menu font8px">
+                        PROFIL
+                    </div>
+                    <div class="menu font10">
+                        <!-- bold jika di click -->
+                        <div class="isi-menu">
+                            <a href="#" style="text-decoration:none;">
+                                Sejarah
+                            </a>
+                            <div class="hr-profile"></div>
+                        </div>
+                        <div class="isi-menu">
+                            <a href="#" style="text-decoration:none;">
+                                Visi dan Misi
+                            </a>
+                            <div class="hr-profile"></div>
+                        </div>
+                        <div class="isi-menu">
+                            <a href="#" style="text-decoration:none;">
+                                Struktur Organisasi
+                            </a>
+                            <div class="hr-profile"></div>
+                        </div>
+                        <div class="isi-menu">
+                            <a href="#" style="text-decoration:none;">
+                                Prestasi
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div> -->
 
 @endsection
